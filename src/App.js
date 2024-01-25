41e277341045bc7168736299293509d51ce98bd3
Hussain-shahg <m.hussain103shah@gmail.com>
2024-01-25 14:37:41 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import ConnectMetamaskUsingEth from "./ConnectMetamaskUsingEth";
+import ConnectMetamaskUsingWalletConnect from "./ConnectMetamaskUsingWalletConnect";
+import QRCodeGenerator from "./QRCodeGenerator";
+
+function App() {
+  // {
+  //   return <ConnectMetamaskUsingEth /> 
+  // }
+
+  
+  return (
+    <div className="App">
+      <h1>Connect MetaMask</h1>
+      {/* <ConnectMetamaskUsingWalletConnect /> */}
+      {/* <QRCodeGenerator /> */}
+      <br />
+    </div>
+  );
+}
+
+export default App;
